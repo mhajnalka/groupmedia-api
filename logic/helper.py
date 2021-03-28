e5001122072670b7503af45c90027f1aa418abd7
markihajnalka <markihajni@gmail.com>
2021-03-28 08:01:32 +0200

    General value checking

--- logic/helper.py
+++ logic/helper.py
@@ -1,4 +1,5 @@
 import inspect
+from datetime import date
 
 
 def fill_data(classname, json):
@@ -8,3 +9,12 @@ def fill_data(classname, json):
         if not key.startswith('_') and not key.startswith('ref_') and key not in request_data.keys():
             request_data[key] = ""
     return request_data
+
+
+def value_chk(request_data, *args):
+    for arg in args.keys():
+        if (isinstance(request_data[arg], int) and request_data[arg] is 0) or (
+                isinstance(request_data[arg], str) and request_data[arg] is "") or \
+                (isinstance(request_data[arg], date) and request_data[arg] is ""):
+            return f"{args[arg].capitalize()} must be given."
+    return ""
