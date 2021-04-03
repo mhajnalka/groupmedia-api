c243327ffb8a0124e796f26b9b77cf1aebe32b23
markihajnalka <markihajni@gmail.com>
2021-04-03 23:57:25 +0200

    Update roles

--- logic/rolemng.py
+++ logic/rolemng.py
@@ -42,7 +42,34 @@ def add(req):
 
 # UPDATE
 def update(req):
-    return jsonify(Message="Role not found"), 404
+    if 'role_id' in req.json:
+        role = Role.query.filter_by(req.json['role_id']).first()
+        if role:
+            if 'event_id' in req.json:
+                if eventmng.exists(req.json['event_id']):
+                    role.event_id = req.json['event_id']
+                else:
+                    return jsonify(message="Event not found."), 404
+            if 'emp_id' in req.json:
+                if employeemng.exists(req.json['emp_id']):
+                    role.responsible_id = req.json['emp_id']
+                else:
+                    return jsonify(message="Employee not found."), 404
+            if 'perm_id' in req.json:
+                if Permission.query.filter_by(perm_id=req.json['perm_id']).one_or_none:
+                    role.responsible_id = req.json['perm_id']
+                else:
+                    return jsonify(message="Permission not found."), 404
+        try:
+            role_schema.load(req.json)
+            db.session.commit()
+        except (TypeError, ValidationError) as err:
+            return jsonify(message=list(eval(str(err)).values())[0][0]), 401
+        except ValueError as err:
+            return jsonify(message=err), 401
+        return jsonify(message="Role has been updated"), 201
+    else:
+        return ""
 
 
 # DELETE
