1544e09dbb37ad08d6f2b3eb0c70a0385f7acf6f
markihajnalka <markihajni@gmail.com>
2021-04-03 13:08:15 +0200

    Seeding initial data

--- app.py
+++ app.py
@@ -11,13 +11,9 @@ db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
 from controllers import routes_blueprint
-
 app.register_blueprint(routes_blueprint)
 
-
-from models import Employee
-from schemas import EmployeeSchema
-from marshmallow import ValidationError
+import initdata
 
 
 @app.cli.command('db_create')
@@ -36,65 +32,17 @@ def db_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
+    app_company = initdata.company
+    app_test_user = initdata.test_user
+    app_permission_r = initdata.read_permission
+    app_permission_w = initdata.write_permission
 
-    test_user = Employee(emp_id=1,
-                         username='my_test_user',
-                         password='123',
-                         firstname='My Test',
-                         lastname='User',
-                         email='mytestuser@faszertvanennyiadat.com',
-                         phone='06306660000',
-                         fax='0612223344',
-                         address='Teszt utca 124.',
-                         city='Budapest',
-                         region='Pest megye',
-                         postcode='1034',
-                         country='Magyarország',
-                         lastlogin=datetime.datetime.strptime('2020/03/20', '%Y/%m/%d')
-                         )
-    test_user2 = Employee(emp_id=2,
-                          username='my_test_user2',
-                          password='12345',
-                          firstname='My Test',
-                          lastname='User2',
-                          email='mytestuser2@faszertvanennyiadat.com',
-                          phone='06306660000',
-                          fax='0612223344',
-                          address='Teszt utca 124.',
-                          city='Budapest',
-                          region='Pest megye',
-                          postcode='1034',
-                          country='Magyarország',
-                          lastlogin=datetime.datetime.strptime('2020/03/20', '%Y/%m/%d')
-                          )
-    """
-    request_data = dict()
-    request_data['emp_id'] = "3"
-    request_data['username'] = "TestUser"
-    request_data['password'] = "12344"
-    request_data['firstname'] = "Test"
-    request_data['lastname'] = "User"
-    request_data['email'] = "marhajo@gmail"
-    request_data['phone'] = "0612234455"
-    request_data['fax'] = ""
-    request_data['address'] = "Pilis u. 9."
-    request_data['city'] = "Budapest"
-    request_data['region'] = "Pest megye"
-    request_data['postcode'] = "1034"
-    request_data['country'] = "Hungary"
-
-    employee_schema = EmployeeSchema()
-    try:
-        valid_test = employee_schema.load(request_data)  # load validates the input
-        db.session.add(valid_test)
-    except ValidationError as err:
-        print(err)
-
-"""
-    db.session.add(test_user)
-    db.session.add(test_user2)
+    db.session.add(app_company)
+    db.session.add(app_test_user)
+    db.session.add(app_permission_r)
+    db.session.add(app_permission_w)
     db.session.commit()
-    print('successful database seeding')
+    print('Database has been successfully seeded')
 
 
 if __name__ == '__main__':
---
+++ initdata.py
@@ -0,0 +1,36 @@
+from models import Company, Employee, Permission
+
+
+company = Company(comp_id=1,
+                  name='Media Company',
+                  email='testuser@testmail.com',
+                  phone='06306660000',
+                  fax='0612223344',
+                  web='www.mediacompanytest.com',
+                  address='Teszt utca 124.',
+                  city='Budapest',
+                  region='Pest megye',
+                  postcode='1034',
+                  country='Magyarország'
+                  )
+
+test_user = Employee(emp_id=1,
+                     username='testuser',
+                     password='Pw12345',
+                     firstname='Test',
+                     lastname='User',
+                     email='testuser@testmail.com',
+                     phone='06306660000',
+                     fax='0612223344',
+                     address='Teszt utca 124.',
+                     city='Budapest',
+                     region='Pest megye',
+                     postcode='1034',
+                     country='Magyarország'
+                     )
+
+read_permission = Permission(perm_id=1,
+                             name='READ')
+
+write_permission = Permission(perm_id=2,
+                              name='WRITE')
