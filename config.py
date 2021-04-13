5146ee9ff77707d4709ed91958adbeb7b94acc41
markihajnalka <markihajni@gmail.com>
2021-04-13 18:10:26 +0200

    JWT expiration has been set for 24h

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
+import datetime
 import os
-import uuid
 
 
 class Config:
@@ -8,4 +8,5 @@ class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'groupmedia.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = str(uuid.uuid1())
+    JWT_SECRET_KEY = '4310583bffb24e8b9e1db6081d321660'  # uuid.uuid4().hex
+    JWT_EXPIRATION_DELTA = datetime.timedelta(days=1)
