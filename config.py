1ceb42b889a21b3cd4a1f32b454ec9d6f46e73cf
markihajnalka <markihajni@gmail.com>
2021-04-27 19:28:45 +0200

    Upload path added

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ import os
 
 class Config:
 
-    # Database
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'groupmedia.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = '4310583bffb24e8b9e1db6081d321660'  # uuid.uuid4().hex
     JWT_EXPIRATION_DELTA = datetime.timedelta(days=1)
+    UPLOAD_DIR = '\\files'
+    UPLOAD_EXTENSIONS = {'txt', 'pdf', 'jpg', 'jpeg', 'png'}
